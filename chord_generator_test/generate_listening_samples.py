64aa80dcb74b5b531483ad6718cae989de417aa5
Askedalen <benjaminkh98@gmail.com>
2022-04-28 15:28:32 +0200

    .

--- chord_generator_test/generate_listening_samples.py
+++ chord_generator_test/generate_listening_samples.py
@@ -9,7 +9,7 @@ import midi_functions as mf
 import pretty_midi
 import os
 import time
-from MusicGenerator import MusicGenerator
+from MusicGenerator import MusicGenerator, BaselineMusicGenerator
 
 if conf.testing:
     chord_model_filename = "chord_generator_test\\results\\tests\\full_model\models\chord_model.pb"
@@ -20,23 +20,25 @@ else:
     poly_model_filename = "results/tests/d220422_t1154/models/poly_model.pb"
     baseline_model_filename = "results/tests/baseline_d220419_t1610/model.pb"
 
-chord_model = load_model(chord_model_filename)
-poly_model = load_model(poly_model_filename)
+#chord_model = load_model(chord_model_filename)
+#poly_model = load_model(poly_model_filename)
+baseline_model = load_model(baseline_model_filename)
 
-threshold = 0.08
+threshold = 0.18
 
-generator = MusicGenerator(chord_model=chord_model, poly_model=poly_model, threshold=threshold)
+#generator = MusicGenerator(chord_model=chord_model, poly_model=poly_model, threshold=threshold)
+generator = BaselineMusicGenerator(baseline_model, threshold=threshold)
 
-melody_filenames = pickle.load(open('test_filenames.pickle', 'rb'))
+#melody_filenames = pickle.load(open('test_filenames.pickle', 'rb'))
 
-melody_filenames = melody_filenames[50:100]
+#melody_filenames = melody_filenames[50:100]
 
-#melody_filenames = ['33e5a8cf07044399f6b99635aee74244.pickle', '532c7ba6291d68c7803035f193a52b76.pickle', '817ce7dfec1502645ef1aa56dcf8fb4b.pickle']
+melody_filenames = ['33e5a8cf07044399f6b99635aee74244.pickle', '532c7ba6291d68c7803035f193a52b76.pickle', '8c8f35129e6f36d33bfa37c508b33c1e.pickle']
 
 for random_song in melody_filenames:
     generator.reset()
     folder_name = os.path.basename(random_song).replace('.pickle', '')
-    if os.path.exists(os.path.join(conf.music_gen_dir, 'test', folder_name)):
+    if os.path.exists(os.path.join(conf.music_gen_dir, 'baseline', folder_name)):
         print(f'{folder_name} already exists')
         continue
     print(f"Generating accompaniment for {folder_name}")
@@ -64,12 +66,12 @@ for random_song in melody_filenames:
     melody_midi_solo = pretty_midi.PrettyMIDI()
     melody_midi_solo.instruments.append(melody_midi)
 
-    os.mkdir(os.path.join(conf.music_gen_dir, 'test', folder_name))
+    os.mkdir(os.path.join(conf.music_gen_dir, 'baseline', folder_name))
 
-    poly_midi_path = os.path.join(conf.music_gen_dir, 'test', folder_name, 'only_comp.mid')
-    poly_melody_path = os.path.join(conf.music_gen_dir, 'test', folder_name, 'comp_and_melody.mid')
-    melody_solo_path = os.path.join(conf.music_gen_dir, 'test', folder_name, 'only_melody.mid')
-    chord_path = os.path.join(conf.music_gen_dir, 'test', folder_name, 'chord_sequence.txt')
+    poly_midi_path = os.path.join(conf.music_gen_dir, 'baseline', folder_name, 'only_comp.mid')
+    poly_melody_path = os.path.join(conf.music_gen_dir, 'baseline', folder_name, 'comp_and_melody.mid')
+    melody_solo_path = os.path.join(conf.music_gen_dir, 'baseline', folder_name, 'only_melody.mid')
+    chord_path = os.path.join(conf.music_gen_dir, 'baseline', folder_name, 'chord_sequence.txt')
 
     midi_output.write(poly_midi_path)
     midi_output.instruments.append(melody_midi)
